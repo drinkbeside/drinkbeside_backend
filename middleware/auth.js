5244843c59f82035d4157c6ac4d264c52309331e
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-06-19 12:34:57 +0300

    authorization fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,17 @@
+require('dotenv').confg();
+const jwt = require('jsonwebtoken');
 const asyncRedis = require('async-redis');
 const redis = asyncRedis.createClient();
 
 module.exports.authorize = async (req, res, next) => {
-  return next();
   const id = req.headers.id;
   const token = req.headers.token;
   const savedToken = await redis.get(id);
-  if(token === savedToken && jwt.verify(token)) return next();
-  return res.json({
+  const correctToken = token === savedToken;
+  const verifiedToken = jwt.verify(token, process.env.SECRET);
+  if(!correctToken || !verifiedToken) return res.json({
     error: 'Ошибка доступа по токену, вы должны быть авторизованы',
     data: null
   });
+  next();
 };
