0015838a39aea0593c8e4890494f0ab562e63d69
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-07-16 01:45:32 +0300

    fix

--- database/postgres.js
+++ database/postgres.js
@@ -20,6 +20,7 @@ module.exports.userByPhone = (phone = null) => {
         done();
         if (err) return resolve(null);
         const user = result.rows[0];
+        if(!user.id) return resolve(user);
         const rating = await self.ratingByID(user.id);
         return resolve({
           ...user,
