7f3b5f93117d98736cb98488d8bef477887afc26
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-07-19 22:53:30 +0300

    investigating /parties

--- index.js
+++ index.js
@@ -114,9 +114,11 @@ app.get('/user/:id', async (req, res) => {
 app.get('/parties', authorize, async (req, res) => {
   const id = Number.parseInt(req.headers.id);
   const parties = await fetchParties(id);
+  console.log(parties);
   const partiesFormatted = await parties.map(async party => {
     const partyID = party.id;
     const list = await guestList(partyID, id);
+    console.log(list);
     return {
       ...party,
       guests: list.length
