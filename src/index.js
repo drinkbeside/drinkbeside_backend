a5916e9a96c424529392c38f0bf1254e503d285e
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-08-22 22:05:24 +0300

    URIs logic fixed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 import bodyparser from 'body-parser';
 import multer from 'multer';
 // system consts
-const app    = express();
+const app = express();
 const upload = multer();
 const config = process.env;
 // custom functions
@@ -25,29 +25,29 @@ app.post('/auth/confirm', router.confirmCode);
 app.post('/auth/refresh', router.refresh);
 
 app.get('/friends/:id',          auth, router.friends);
-app.post('/friends/add/:id',     auth, router.addFriend);
-app.post('/friends/confirm/:id', auth, router.confirmFriend);
-app.post('/friends/decline/:id', auth, router.declineFriend);
-app.post('/friends/remove/:id',  auth, router.removeFriend);
+app.post('/friends/:id/add',     auth, router.addFriend);
+app.post('/friends/:id/confirm', auth, router.confirmFriend);
+app.post('/friends/:id/decline', auth, router.declineFriend);
+app.post('/friends/:id/remove',  auth, router.removeFriend);
 
+app.get('/user/:id/rate',         auth, router.rateGuest);
 app.get('/user/:id',              auth, router.seekUserByID);
 app.post('/user/app',             auth, router.appInvite);
 app.post('/user/update_info',     auth, router.updateUserInfo);
 app.post('/user/update_avatar',   auth, upload.single('image'), router.updateAvatar);
 app.post('/user/update_location', auth, router.updateUserLocation);
 
-app.get('/parties',           auth, router.parties);
-app.get('/party/:id',         auth, router.partyByID);
-app.get('/party/guests/:pid', auth, router.guestList);
-app.get('/party/rate/:id',    auth, router.rateGuest);
-app.post('/party/rate',       auth, router.rate);
-app.post('/party/join',       auth, router.joinParty);
-app.post('/party/leave',      auth, router.leaveParty);
-app.post('/party/create',     auth, router.createParty);
-app.post('/party/modify',     auth, router.modifyParty);
-app.post('/party/suspend',    auth, router.suspendParty);
-app.post('/party/invite',     auth, router.inviteToParty);
-app.post('/party/kick',       auth, router.kickGuest);
+app.get('/parties',          auth, router.parties);
+app.get('/party/:id/guests', auth, router.guestList);
+app.get('/party/:id',        auth, router.partyByID);
+app.post('/party/rate',      auth, router.rate);
+app.post('/party/join',      auth, router.joinParty);
+app.post('/party/leave',     auth, router.leaveParty);
+app.post('/party/create',    auth, router.createParty);
+app.post('/party/modify',    auth, router.modifyParty);
+app.post('/party/suspend',   auth, router.suspendParty);
+app.post('/party/invite',    auth, router.inviteToParty);
+app.post('/party/kick',      auth, router.kickGuest);
 
 app.get('/seek',    auth, router.seek);
 app.post('/places', auth, router.places);
