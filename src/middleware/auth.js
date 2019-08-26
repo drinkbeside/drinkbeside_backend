0906c4472e812fec840e5fb77ae8c1daa9614d28
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-08-26 09:35:17 +0300

    little verbosity

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,10 +7,13 @@ const redis = asyncRedis.createClient();
 export const authorize = async (req, res, next) => {
   const token = req.headers.access;
   return await jwt.verify(token, process.env.SECRET, async (err, decoded) => {
-    if(err) return res.status(403).json({
-      error: 'Ошибка доступа по токену, токен истек',
-      data: null
-    });
+    if(err) {
+      console.log(err);
+      return res.status(403).json({
+        error: 'Ошибка доступа по токену, токен истек',
+        data: null
+      });
+    }
     let user = await redis.get(token);
     if(!user) return res.status(401).json({
       error: 'Ошибка, такого токена не существует',
