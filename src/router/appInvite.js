4a95d2b69c7920b85acb61c845183ed3115ee189
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-09-01 10:19:38 +0300

    invitation to app implemented

--- src/router/appInvite.js
+++ src/router/appInvite.js
@@ -1,13 +1,16 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 const config = process.env;
+import jwt from 'jsonwebtoken';
 
 import axios from 'axios';
 
 export const appInvite = async (req, res) => {
+  const { user } = await jwt.verify(req.headers.access, config.SECRET);
   const phone = req.body.phoneNumber;
   const invitation = config.INVITATION_MESSAGE_TEMPLATE
-    .replace('<invitation_text>', config.INVITATION_MESSAGE_TEXT_RU);
+    .replace('<invitation_text>', config.INVITATION_MESSAGE_TEXT_RU)
+    .replace('<user_fullname>', `${user.fname} ${user.lanme ? user.lname + ' ' : ''}`);
   const toSend = config.DEF_URL
     .replace('<phones>', phone)
     .replace('<message>', invitation);
