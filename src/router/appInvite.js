6716df88057caab415fc184d052d7ba46df55399
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-09-01 10:27:08 +0300

    escaping added

--- src/router/appInvite.js
+++ src/router/appInvite.js
@@ -8,9 +8,9 @@ import axios from 'axios';
 export const appInvite = async (req, res) => {
   const { user } = await jwt.verify(req.headers.access, config.SECRET);
   const phone = req.body.phoneNumber;
-  const invitation = config.INVITATION_MESSAGE_TEMPLATE
+  const invitation = encodeURI(config.INVITATION_MESSAGE_TEMPLATE
     .replace('<invitation_text>', config.INVITATION_MESSAGE_TEXT_RU)
-    .replace('<user_fullname>', `${user.fname} ${user.lanme ? user.lname + ' ' : ''}`);
+    .replace('<user_fullname>', `${user.fname} ${user.lanme ? user.lname + ' ' : ''}`));
   const toSend = config.DEF_URL
     .replace('<phones>', phone)
     .replace('<message>', invitation);
