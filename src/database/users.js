7ff5e3e97b4a455dae1b1c02a85e855518dc6954
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-09-14 23:43:30 +0300

    confirmCode fix

--- src/database/users.js
+++ src/database/users.js
@@ -6,10 +6,18 @@ export const userByPhone = (phone = null) => {
   return new Promise(resolve => {
     if (!phone) return resolve(null);
     return pool.connect((err, client, done) => {
-      if (err) return resolve(null);
+      if (err) {
+        console.log('ОШИБКА ПУЛА');
+        console.log(err);
+        return resolve(null);
+      }
       client.query(`SELECT * FROM users WHERE phone = '${phone}'`, async (err, result) => {
         done();
-        if (err) return resolve(null);
+        if (err) {
+          console.log('ОШИБКА ЗАПРОСА');
+          console.log(err);
+          return resolve(null);
+        }
         const user = result.rows[0];
         console.log(`А в БД user: ${user}`);
         if(!user) return resolve(null);
