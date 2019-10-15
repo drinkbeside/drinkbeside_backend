f61fac33ee8c4797bfbb43f149b9bbcb1294bfd4
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-10-15 17:06:54 +0300

    city decoder injected to updateLocation

--- src/router/updateUserLocation.js
+++ src/router/updateUserLocation.js
@@ -3,13 +3,15 @@ dotenv.config();
 const config = process.env;
 
 import jwt from 'jsonwebtoken';
+import { decode } from '../middleware/citydecoder';
 import { updateLocation } from '../database';
 
 export const updateUserLocation = async (req, res) => {
   const { user } = await jwt.verify(req.headers.access, config.SECRET);
   const userID = user.id;
   const city = req.body.city;
-  const done = await updateLocation(userID, city);
+  const decoded = decode(city);
+  const done = await updateLocation(userID, decoded);
   if (!done) return res.status(500).json({
     data: null,
     error: 'Ошибка изменения города пользователя'
