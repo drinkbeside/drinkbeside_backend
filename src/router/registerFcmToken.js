abd1a463650c8c7409d82ee4186fd9fae92afb7e
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-11-21 14:19:27 +0300

    updating code-style, fixing issues

--- src/router/registerFcmToken.js
+++ src/router/registerFcmToken.js
@@ -2,20 +2,20 @@ import { updateUserFcmToken } from '../database';
 import { userByID } from '../database/users.js'
 
 export const registerFcmToken = async (req, res) => {
-  const data = req.body;
-  const token = data.token;
-  const id = Number.parseInt(data.userId);
+  const { userId, token } = req.body;
+  const id = Number.parseInt(userId);
   const user = await userByID(id);
-  console.log(id, user);
-  if (user) {
-    const updatedUser = await updateUserFcmToken(id, token);
-    if (updatedUser) return res.json({
-      error: null,
-      data: updatedUser
-    });
-  }
-  res.status(500).json({
+  if (!user) return res.status(500).json({
+    error: 'Ошибка, такого пользователя не существует',
+    data: null
+  });
+  const updatedUser = await updateUserFcmToken(id, token);
+  if (!updatedUser) return res.status(500).json({
     error: 'Ошибка обновления данных пользователя',
     data: null
+  });
+  return res.json({
+    error: null,
+    data: updatedUser
   });
 };
