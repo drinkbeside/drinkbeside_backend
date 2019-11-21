342f930aff6eec5b3b077fc6e1f5e4e22cb1424a
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-11-21 04:14:46 +0300

    fixing

--- src/router/addFriend.js
+++ src/router/addFriend.js
@@ -9,6 +9,7 @@ export const addFriend = async (req, res) => {
   const uid = user.id;
   const id = req.params.id;
   const added = await toFriends(uid, id);
+  console.log(added);
   if(!added) return res.status(500).json({
     data: null,
     error: 'Ошибка добавления в друзья, попробуйте позже'
