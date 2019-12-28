ea7b942814f09b36684f7be422bb3f337bf236a6
Danil Kovalev <hhadevs@gmail.com>
2019-12-28 09:22:06 +0300

    fucking my brain

--- src/router/fetchShops.js
+++ src/router/fetchShops.js
@@ -6,7 +6,7 @@ import jwt from 'jsonwebtoken';
 var GooglePlaces = require("googleplaces");
 var googlePlaces = new GooglePlaces("AIzaSyD_HUegxAl7wfn5nQtHtDDh0gut-1nBtDM", 'json'); // todo: // pls move this key to external safe place, i dunno where it is
 
-export const shops = async (req, res) => {
+export const fetchShops = async (req, res) => {
   const { user } = await jwt.verify(req.headers.access, config.SECRET);
   const citykey = decode(user.city);
   const translatedCity = encodeEnglish(citykey);
